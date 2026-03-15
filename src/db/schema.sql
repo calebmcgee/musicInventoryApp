22bf95bbd2412d9ae4542b2076df37a92bda582b
Caleb <calebmcgee06@gmail.com>
2026-03-15 15:16:39 -0400

    Troubleshoot deployment

--- src/db/schema.sql
+++ src/db/schema.sql
@@ -21,7 +21,7 @@ CREATE TABLE songs (
     name TEXT NOT NULL,
     album_id INTEGER REFERENCES albums(id),
     genre TEXT,
-    rating INTEGER CHECK (rating BETWEEN 0 AND 10),
+    rating INTEGER CHECK (rating BETWEEN 0 AND 10)
 );
 
 CREATE TABLE song_artists(
